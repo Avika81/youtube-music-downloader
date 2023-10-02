a5bdcad9d715e69e6c7a4947fd339dc4d656d34a
Avi Kadria <avi.kadria3@gmail.com>
2023-10-02 19:29:17 +0300

    works for me, idk about you all :)

--- download_music_youtube.py
+++ download_music_youtube.py
@@ -1,10 +1,12 @@
+from multiprocessing import Pool, TimeoutError
+from threading import Thread
 from pytube import YouTube
 from pytube import Playlist
 import json
 import os
 from pathlib import Path
 
-def youtube2mp3(url,outdir):
+def youtube2mp3(url, outdir):
     yt = YouTube(url)
     try:
         video = yt.streams.filter(only_audio=True).first()
@@ -12,15 +14,10 @@ def youtube2mp3(url,outdir):
         print(f'Failed downloading: {url} - {yt.title}')
         return
     destination = outdir or '.'
-    out_file = video.download(output_path=destination)
-    base, ext = os.path.splitext(out_file)
-    new_file = base + '.mp3'
-    os.rename(out_file, new_file)
-    print(yt.title + " has been successfully downloaded.")
+    video.download(output_path=destination, filename=f'{yt.author} - {yt.title}.mp4'.replace('?', '').replace('|', '-'))
 
 
 def all_urls_from_youtube_playlist(url_playlist):
-    # Retrieve URLs of videos from playlist
     playlist = Playlist(url_playlist)
     print('Number Of Videos In playlist: %s' % len(playlist.video_urls))
 
@@ -41,17 +38,4 @@ def download_playlist(url,outdir):
         youtube2mp3(songs_url, dest_dir)
 
 
-with open('songs.json') as rf:
-    songs_to_download = json.load(rf)
 
-# if __name__ == "__main__":
-download_playlist(
-    url=r"https://www.youtube.com/watch?v=gl1aHhXnN1k&list=PLcc2EHxL-4QRSjIQfFWNX3zasjT76drjq",
-    outdir=r"./ariana_grande"
-    )
-
-    # for url in songs_to_download:
-    #     try:
-    #         youtube2mp3(url, r"C:\Temp\2023-09-26\songs")
-    #     except Exception as e:
-    #         print(f'Failed to download url {url}')
