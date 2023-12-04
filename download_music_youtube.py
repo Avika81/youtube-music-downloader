ca5f43e8eb5fb9861b682537edbe600e9bec2bc9
Avi Kadria <avi.kadria3@gmail.com>
2023-12-04 10:29:08 +0200

    black + joblib - cache - faster gooder :)

--- download_music_youtube.py
+++ download_music_youtube.py
@@ -1,25 +1,31 @@
+import joblib
 from multiprocessing import Pool, TimeoutError
 from threading import Thread
 from pytube import YouTube
 from pytube import Playlist
-import json
-import os
 from pathlib import Path
 
-def youtube2mp3(url: str, outdir: str) -> None:
+memory = joblib.Memory('./.cache', verbose=0)
+
+def _youtube2mp3(url: str, outdir: str) -> None:
     yt = YouTube(url)
     try:
         video = yt.streams.filter(only_audio=True).first()
     except:
-        print(f'Failed downloading: {url} - {yt.title}')
+        print(f"Failed downloading: {url} - {yt.title}")
         return
-    destination = outdir or '.'
-    video.download(output_path=destination, filename=f'{yt.author} - {yt.title}.mp4'.replace('?', '').replace('|', '-').replace('/', '-'))
-
+    destination = outdir or "."
+    filename = f"{yt.author} - {yt.title}.mp4".replace("?", "").replace("|", "-").replace("/", "-")
+    video.download(
+        output_path=destination,
+        filename=filename,
+    )
+    return filename
+youtube2mp3 = memory.cache(_youtube2mp3)
 
 def all_urls_from_youtube_playlist(url_playlist):
     playlist = Playlist(url_playlist)
-    print('Number Of Videos In playlist: %s' % len(playlist.video_urls))
+    print("Number Of Videos In playlist: %s" % len(playlist.video_urls))
 
     urls = []
     for url in playlist:
@@ -28,7 +34,7 @@ def all_urls_from_youtube_playlist(url_playlist):
     return urls
 
 
-def download_playlist(url,outdir):
+def download_playlist(url, outdir):
     for songs_url in all_urls_from_youtube_playlist(url):
         dest_dir = Path(outdir) / Playlist(url).title
         try:
@@ -36,6 +42,3 @@ def download_playlist(url,outdir):
         except:
             pass
         youtube2mp3(songs_url, dest_dir)
-
-
-
