08e044962d36bb2cfadafd725a7b6e2c8ba6ce9c
Avi Kadria <avi.kadria3@gmail.com>
2025-09-19 22:07:02 +0300

    shay asked and I'm helpful

---
+++ add_song.py
@@ -0,0 +1,34 @@
+from pytubefix import YouTube
+import sys
+from collections import Counter
+
+from download_music_youtube import yt_to_filename  # type: ignore
+from sync import save, read, sort_json  # type: ignore
+
+
+def remove_duplicates(songs_to_download):
+    duplicate_values = [
+        k for k, v in Counter(songs_to_download.values()).items() if v > 1
+    ]
+
+    for value in duplicate_values:
+        for u in songs_to_download:
+            if songs_to_download[u] == value:
+                songs_to_download.pop(u)
+                break
+
+
+def main():
+    url = sys.argv[1]
+    songs_data = read()
+    if url not in songs_data:
+        fname = yt_to_filename(YouTube(url, use_po_token=True))
+        if fname not in songs_data.values():
+            songs_data[url] = fname
+            remove_duplicates(songs_data)
+            save(songs_data)
+            sort_json()
+
+
+if __name__ == "__main__":
+    main()
---
+++ sync.py
@@ -0,0 +1,49 @@
+import os
+import tqdm
+import json
+from download_music_youtube import sync_from_json, yt_to_filename, add_tag
+from pytubefix import YouTube
+
+OUTDIR = r"C:\BackUp DOK\Extra\music\avi"
+JSON_FILE = r"C:\checkouts\youtube-music-downloader\avi_songs_dict.json"
+
+
+def save(json_data):
+    with open(JSON_FILE, "w", encoding="utf-8") as of:
+        of.write(json.dumps(json_data, indent=4, ensure_ascii=False))
+
+
+def read():
+    with open(JSON_FILE, encoding="utf-8") as rf:
+        return json.load(rf)
+
+
+def add_tags():
+    songs_to_download = read()
+    for url, fname in tqdm.tqdm(songs_to_download.items()):
+        add_tag(os.path.join(OUTDIR, fname), YouTube(url, use_po_token=True))
+
+
+def sync_titles():
+    songs_to_download = read()
+    for url, fname in tqdm.tqdm(songs_to_download.items()):
+        yt = YouTube(url, use_po_token=True)
+        new_fname = yt_to_filename(yt)
+        if new_fname != fname:
+            print(f"New fname is different: {new_fname} ||| vs ||| {fname}")
+            songs_to_download[url] = new_fname
+    if input("Resave after changes? (Y/N)").lower().strip() == "y":
+        save(songs_to_download)
+    return songs_to_download
+
+
+def sort_json():
+    with open(JSON_FILE, encoding="utf-8") as rf:
+        lines = rf.read().split("\n")
+        lines = [line for line in lines if line.strip("{").strip("}").strip() != ""]
+        lines.sort(key=lambda x: x.split(': "')[1])
+    save(json.loads("{" + ",".join([line.rstrip(",") for line in lines]) + "}"))
+
+
+if __name__ == "__main__":
+    sync_from_json(read(), OUTDIR)
